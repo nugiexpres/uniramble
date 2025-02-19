35526c61bd46df41899f9c98c27113f12aee2a64
zkpedia <133411615+nugiexpres@users.noreply.github.com>
2025-02-19 21:55:20 +0700

    Update board.tsx
    Type error: Parameter 'item' implicitly has an 'any' type.

--- packages/nextjs/components/board/board.tsx
+++ packages/nextjs/components/board/board.tsx
@@ -130,7 +130,7 @@ export const Board = () => {
           </div>
           <div className="relative mt-3 bg-green-300" style={{ width: "530px", height: "530px" }}>
             {gridData &&
-              gridData.map((item, index) => (
+              gridData.map((item: { id: number; typeGrid: string }, index) => (
                 <div
                   key={index}
                   className={
